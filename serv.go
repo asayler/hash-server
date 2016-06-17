4bb5ce7d83cf7269fc4f0f0dbbb92d8ce973eadf
Andy Sayler <andy.sayler@gmail.com>
2016-06-17 10:35:23 -0600

    Added hashing functionality

--- serv.go
+++ serv.go
@@ -1,16 +1,47 @@
 package main
 
 import (
+	"crypto/sha512"
+	"encoding/base64"
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 )
+
+const (
+	SLEEP = 5 * time.Second
+)
+
+func hashRequest(w http.ResponseWriter, r *http.Request) {
+
+	// Request Info
+	log.Printf("Method: %s\n", r.Method)
+	log.Printf("Header: %s\n", r.Header)
+
+	// Extract Password
+	password := r.FormValue("password")
+	log.Printf("Password: %s\n", password)
+	password_bytes := []byte(password)
+
+	// Hash Password
+	hash_bytes := sha512.Sum512(password_bytes)
+	hash := base64.StdEncoding.EncodeToString(hash_bytes[:])
+	log.Printf("Hash: %s\n", hash)
+
+	// Sleep
+	log.Printf("Sleeping for %d seconds", SLEEP/time.Second)
+	time.Sleep(SLEEP)
+
+	// Reply
+	log.Printf("Responding")
+	fmt.Fprintf(w, "%s\n", hash)
+
+}
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, world")
-	})
-
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", hashRequest)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
